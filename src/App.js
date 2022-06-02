f2429b68949c08515156a49c6f987f5317c924c7
nbvishal@noborderz.com <vishal.thoughtwin@gmail.com>
2022-06-02 11:49:45 +0530

    mounting method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Property
+
+  // Constructor
+  constructor(props){
+    super(props);
+    this.state = {
+      friend : "Shivkanya"
+    }
+  }
+
+  //Methods
+static getDevrivedStateFromProps(){
+  console.log("hello");
+  return null;
 }
 
-export default App;
+  render() {
+    return (
+      <div className="App App-header">
+        <h1>{this.state.friend}<br />{this.props.friend2}</h1>
+      </div>
+    );
+  }
+}
+
