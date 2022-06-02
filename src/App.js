4f59126e02366b12c85a98e5ba1736d9aecaaee7
nbvishal@noborderz.com <vishal.thoughtwin@gmail.com>
2022-06-02 13:06:58 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 export default class App extends Component {
-
   // Property
 
   // Constructor
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      friend : "Shivkanya"
-    }
+      friend: "Shivkanya",
+    };
   }
 
   //Methods
-static getDevrivedStateFromProps(){
-  console.log("hello");
-  return null;
-}
+  static getDerivedStateFromProps(props, state) {
+    console.log("hello", props, state);
+    if(props.friend2 !== state.friend)
+    return null;
+  }
+
+  clickHere = () => {
+    // console.log("clicked");
+    this.setState({
+      age :"24"
+    })
+    
+    var x = document.getElementById("k").innerHTML = this.props.friend2;
+    console.log(x);
+  };
+
+  componentDidMount(){
+    console.log("k");
+    let y =document.getElementById("k").innerHTML = this.state.friend;
+    console.log(y);
+  }
 
   render() {
     return (
       <div className="App App-header">
-        <h1>{this.state.friend}<br />{this.props.friend2}</h1>
+        <h1 id="k">
+          {this.state.friend} 
+          <br />
+          {this.props.friend2}
+          <br />
+        </h1>
+        <button onClick={this.clickHere}>Click Here</button>
       </div>
     );
   }
 }
-
